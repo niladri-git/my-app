ba6bed5f26cef656076efd3a9bcf4d2417cf8459
Niladri Datta <niladri-git@users.noreply.github.com>
2019-06-21 21:22:09 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App. #1
+ * Unit test for simple App. #2
  */
 public class AppTest 
     extends TestCase
